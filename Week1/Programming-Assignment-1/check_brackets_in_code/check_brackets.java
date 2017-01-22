859c833a2a96f252a71003252a470bef7c7af258
Joshua <tantohyung@gmail.com>
2017-01-22 11:22:31 +0800

    Complete assingment 1 problem 1

---
+++ Week1/Programming-Assignment-1/check_brackets_in_code/check_brackets.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+class Bracket {
+    Bracket(char type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    boolean Match(char c) {
+        if (this.type == '[' && c == ']')
+            return true;
+        if (this.type == '{' && c == '}')
+            return true;
+        if (this.type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+}
+
+class check_brackets {    
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                // Process opening bracket
+                Bracket b = new Bracket(next, position);
+                opening_brackets_stack.push(b);
+            }
+
+            if (next == ')' || next == ']' || next == '}') {
+                if(opening_brackets_stack.empty() || opening_brackets_stack.pop().Match(next) != true) {
+                    System.out.println(position + 1);
+                    return;
+                }
+            }
+        }
+
+        if(opening_brackets_stack.empty()) {
+            System.out.println("Success");
+        } else {
+            System.out.println(opening_brackets_stack.pop().position + 1);
+        }
+    }
+}
