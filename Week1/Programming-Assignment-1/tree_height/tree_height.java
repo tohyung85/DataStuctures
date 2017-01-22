77bc256a0308114d871e9610b7b4910e2f0471b9
Joshua <tantohyung@gmail.com>
2017-01-22 15:55:29 +0800

    Complete assignment 1 problem 2

--- Week1/Programming-Assignment-1/tree_height/tree_height.java
+++ Week1/Programming-Assignment-1/tree_height/tree_height.java
@@ -23,27 +23,82 @@ public class tree_height {
 	public class TreeHeight {
 		int n;
 		int parent[];
+		Node root;
+		Node[] nodes;
 		
 		void read() throws IOException {
+
 			FastScanner in = new FastScanner();
 			n = in.nextInt();
 			parent = new int[n];
+			nodes = new Node[n];			
 			for (int i = 0; i < n; i++) {
-				parent[i] = in.nextInt();
+				parent[i] = in.nextInt(); // create array with parent node as value
+
+				Node node = new Node(); // create tree node
+				node.value = i; // Value of node is simply the sequence which the integers are given
+				nodes[i] = node; // set in such a way that the index of nodes array corresponds to the value
 			}
+
+			for(int i = 0; i<n; i++) { // loop through the parent array to set the children of each node.
+				if(parent[i] == -1) {
+					root = nodes[i];
+				} else {
+					nodes[parent[i]].addChild(nodes[i]); // because of node array is index according to it's value, just set it's child accordingly.
+				}
+			}
+
 		}
 
-		int computeHeight() {
-                        // Replace this code with a faster implementation
-			int maxHeight = 0;
-			for (int vertex = 0; vertex < n; vertex++) {
-				int height = 0;
-				for (int i = vertex; i != -1; i = parent[i])
-					height++;
-				maxHeight = Math.max(maxHeight, height);
+		class Node {
+			int value;
+			Stack<Node> children = new Stack<Node>(); // Variable number of children, have to use stack or queue
+
+			void addChild(Node n) {
+				children.push(n);
+			}
+
+			Node getChild() {
+				return children.pop();
+			}
+
+			boolean hasChildren() {
+				return !children.empty();
+			}
+		}		
+
+		Integer computeHeightFast() {
+			Stack<Node> nodeStack = new Stack<Node>(); // node stack to keep track of nodes
+			Stack<Integer> levelStack = new Stack<Integer>(); // Level stack, to keep track of current tree height
+			Integer maxHeight = 1;
+			nodeStack.push(root);
+			levelStack.push(1);
+
+			while(!nodeStack.empty()) { // Interesting point: Queues will enable breadth first, stack enables depth first
+				Node current = nodeStack.pop(); // remove node at top of stack
+				Integer currentHeight = levelStack.pop(); // set current height where the node is at
+				if(currentHeight > maxHeight) maxHeight = currentHeight;
+
+				while(current.hasChildren()) { // while node has children, push it to the node stack. Also push the height where the children are located to top of level stack. (i.e 1 level higher)
+					nodeStack.push(current.getChild());
+					levelStack.push(currentHeight + 1);
+				}
 			}
 			return maxHeight;
 		}
+
+// Slower Naive Algorithm, for each element backtrace until parent while counting height. O(n2)
+		// int computeHeight() {
+  //                       // Replace this code with a faster implementation
+		// 	int maxHeight = 0;
+		// 	for (int vertex = 0; vertex < n; vertex++) {
+		// 		int height = 0;
+		// 		for (int i = vertex; i != -1; i = parent[i])
+		// 			height++;
+		// 		maxHeight = Math.max(maxHeight, height);
+		// 	}
+		// 	return maxHeight;
+		// }		
 	}
 
 	static public void main(String[] args) throws IOException {
@@ -59,6 +114,7 @@ public class tree_height {
 	public void run() throws IOException {
 		TreeHeight tree = new TreeHeight();
 		tree.read();
-		System.out.println(tree.computeHeight());
+		// System.out.println(tree.computeHeight()); for slower algo
+		System.out.println(tree.computeHeightFast());
 	}
 }
