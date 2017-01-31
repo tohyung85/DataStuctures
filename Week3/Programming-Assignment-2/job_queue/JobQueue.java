37c13584a6803aff97f3531dca29bb5735e3107b
Joshua <tantohyung@gmail.com>
2017-01-31 20:32:03 +0800

    Complete assignment 2 problem 2

--- Week3/Programming-Assignment-2/job_queue/JobQueue.java
+++ Week3/Programming-Assignment-2/job_queue/JobQueue.java
@@ -16,20 +16,20 @@ public class JobQueue {
     private PriorityQueue<JobWorker> pq; 
 
     private class JobWorker {
-    int index;
-    long taskEndTime;
+        int index;
+        long taskEndTime;
     }
 
-    private class WorkerComparator implements Comparator<JobWorker> {
-    public int compare(JobWorker a, JobWorker b) {
-      if(a.taskEndTime > b.taskEndTime) return -1;
-      if(a.taskEndTime < b.taskEndTime) return 1;
-      if(a.taskEndTime == b.taskEndTime) {
-        if(a.index < b.index) return 1;
-        if(a.index > b.index) return -1;
-      }
-      return 0;
-    }
+    private class WorkerComparator implements Comparator<JobWorker> { // Comparator function for use with priority queue
+        public int compare(JobWorker a, JobWorker b) {
+          if(a.taskEndTime > b.taskEndTime) return 1; // Return 1 if worker will end task later
+          if(a.taskEndTime < b.taskEndTime) return -1;
+          if(a.taskEndTime == b.taskEndTime) { // If workers end at the same time, sort by index
+            if(a.index < b.index) return -1; 
+            if(a.index > b.index) return 1;
+          }
+          return 0;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,13 +40,15 @@ public class JobQueue {
         numWorkers = in.nextInt();
         int m = in.nextInt();
         jobs = new int[m];
-        for (int i = 0; i < m; ++i) {
-            jobs[i] = in.nextInt();
+        for (int i = 0; i < m; ++i) { // Store job run times in array
+            jobs[i] = in.nextInt();            
+        }
 
-        JobWorker jw = new JobWorker();
-        jw.index = i;
-        jw.taskEndTime = 0;
-        pq.add(jw);
+        for (int i = 0; i < numWorkers; i++) { // Initialize workers by storing index and set initial task end time to 0 before adding to pq
+            JobWorker jw = new JobWorker();
+            jw.index = i;
+            jw.taskEndTime = 0;
+            pq.add(jw);
         }
     }
 
@@ -56,8 +58,7 @@ public class JobQueue {
         }
     }
 
-    private void assignJobs() {
-        // TODO: replace this code with a faster algorithm.
+    private void assignJobs() {// Slow Alogrithm: replace this code with a faster algorithm.        
         long[] nextFreeTime = new long[numWorkers];
         for (int i = 0; i < jobs.length; i++) {
             int duration = jobs[i];
@@ -72,27 +73,25 @@ public class JobQueue {
         }
     }
 
-    private void assignJobsFast() {
-    for(int i = 0; i < jobs.length; i++) {
-          assignedWorker = new int[jobs.length];
-          startTime = new long[jobs.length];
-      JobWorker bestWorker = pq.poll();
-      assignedWorker[i] = bestWorker.index;
-      startTime[i] = bestWorker.taskEndTime;
-          bestWorker.taskEndTime += jobs[i];
-      pq.add(bestWorker);
-    }       
+    private void assignJobsFast() { // Fast algorithm using priority queue
+        for(int i = 0; i < jobs.length; i++) { 
+            JobWorker bestWorker = pq.poll(); // For each job, pop best worker from the stack based on comparator function
+            assignedWorker[i] = bestWorker.index; // set the assigned worker for the task
+            startTime[i] = bestWorker.taskEndTime; // store time job starts
+            bestWorker.taskEndTime += jobs[i]; // update new end time for worker
+            pq.add(bestWorker); // add back to Priority Queue
+        }       
     }
 
     public void solve() throws IOException {
         in = new FastScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
-    pq = new PriorityQueue<JobWorker>(10, new WorkerComparator());
+        pq = new PriorityQueue<JobWorker>(10, new WorkerComparator()); // Initialize prior
         readData();
-        assignedWorker = new int[jobs.length];
-        startTime = new long[jobs.length];
-//        assignJobs();
-    assignJobsFast();
+        assignedWorker = new int[jobs.length]; // For storage of results
+        startTime = new long[jobs.length]; // For storage of results
+//        assignJobs(); // For naive algorithm
+        assignJobsFast();
         writeResponse();
         out.close();
     }
