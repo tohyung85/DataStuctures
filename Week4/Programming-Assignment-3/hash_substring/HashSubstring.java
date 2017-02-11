1d6110cb4668ae1e4b0991399faf539a6f60137a
Joshua <tantohyung@gmail.com>
2017-02-11 16:25:23 +0800

    Solved assignment 3 problem 3

--- Week4/Programming-Assignment-3/hash_substring/HashSubstring.java
+++ Week4/Programming-Assignment-3/hash_substring/HashSubstring.java
@@ -9,10 +9,18 @@ public class HashSubstring {
     private static FastScanner in;
     private static PrintWriter out;
 
+    private static long prime = 100000000003L;
+    private static int multiplier = 31;
+
+    private static long[] hashes;
+
     public static void main(String[] args) throws IOException {
         in = new FastScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
-        printOccurrences(getOccurrences(readInput()));
+        Data input = readInput();
+        // printOccurrences(getOccurrences(input));
+        // out.println();
+        printOccurrences(getOccurrencesFast(input));
         out.close();
     }
 
@@ -22,6 +30,13 @@ public class HashSubstring {
         return new Data(pattern, text);
     }
 
+    private static long hashFunc(String s) {
+        long hash = 0;
+        for (int i = s.length() - 1; i >= 0; --i)
+            hash = (hash * multiplier + s.charAt(i)) % prime;
+        return hash;
+    }
+
     private static void printOccurrences(List<Integer> ans) throws IOException {
         for (Integer cur : ans) {
             out.print(cur);
@@ -29,23 +44,65 @@ public class HashSubstring {
         }
     }
 
-    private static List<Integer> getOccurrences(Data input) {
-        String s = input.pattern, t = input.text;
+    private static List<Integer> getOccurrencesFast(Data input) {
+        String s = input.pattern, t = input.text;        
         int m = s.length(), n = t.length();
-        List<Integer> occurrences = new ArrayList<Integer>();
-        for (int i = 0; i + m <= n; ++i) {
-	    boolean equal = true;
-	    for (int j = 0; j < m; ++j) {
-		if (s.charAt(j) != t.charAt(i + j)) {
-		     equal = false;
- 		    break;
-		}
-	    }
-            if (equal)
-                occurrences.add(i);
-	}
+        hashes = new long[n - m + 1];
+        preComputeHashes(hashes, t, s);
+        long patternHash = hashFunc(s);
+        List<Integer> occurrences = new ArrayList<Integer>();      
+
+        for(int i = 0; i < n-m +1 ; i++) { // Count from 0!!!!
+            if(hashes[i] != patternHash) {
+                continue;
+            } 
+            // If hashes match will need to compare strings incase of conflicts.
+            boolean match = true;
+            for(int j = 0; j < m; j++) { 
+                if(t.charAt(i+j) != s.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            } 
+            if(match) {
+                occurrences.add(i); // This is a O(1) operation. occurences.add(0, i) is a O(N) operation!! Huge difference!
+            }              
+        }
         return occurrences;
     }
+
+    private static void preComputeHashes(long[] hashArr, String text, String pattern) {
+        // Similar to dynamic programming. Find last value and use them to calculate the remaining values.
+        int m = pattern.length(), n = text.length();
+        hashes[hashArr.length - 1] = hashFunc(text.substring(n - m));
+        long y = 1;
+        for(int i = 0; i < m; i++) {
+            y = (y*multiplier) % prime;
+        }
+        for(int i = n - m - 1; i>=0; i--) {
+            // note that due to potential negative remainder, will need to add prime and mod again.
+            hashes[i] = ((multiplier * hashes[i + 1] - text.charAt(i+m)*y + text.charAt(i))%prime + prime) % prime; 
+        }
+    }
+
+// Naive Implementation
+    // private static List<Integer> getOccurrences(Data input) {
+    //     String s = input.pattern, t = input.text;
+    //     int m = s.length(), n = t.length();
+    //     List<Integer> occurrences = new ArrayList<Integer>();
+    //     for (int i = 0; i + m <= n; ++i) {
+	   //  boolean equal = true;
+	   //  for (int j = 0; j < m; ++j) {
+    // 		if (s.charAt(j) != t.charAt(i + j)) {
+    // 		     equal = false;
+    //  		    break;
+    // 		}
+	   //  }
+    //         if (equal)
+    //             occurrences.add(i);
+	   // }
+    //     return occurrences;
+    // }
 
     static class Data {
         String pattern;
